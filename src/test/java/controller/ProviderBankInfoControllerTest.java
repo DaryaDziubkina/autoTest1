5dbd62ab92ff72119f773539c0fde5bd6cfaea68
dasha.dubkina@gmail.com <dasha.dubkina@gmail.com>
2016-04-05 15:33:12 +0300

    change

---
+++ src/test/java/controller/ProviderBankInfoControllerTest.java
@@ -0,0 +1,17 @@
+package controller;
+
+import junit.framework.TestCase;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
+
+public class ProviderBankInfoControllerTest extends TestCase {
+
+    public void testGetProviderInfoList() throws Exception {
+        Client client = ClientBuilder.newClient().register(ProviderBankInfoController.class);
+        javax.ws.rs.core.Response response = client.target("localhost:8080/api/agents/providerInfoList").request(MediaType.APPLICATION_JSON).get();
+        assertEquals("сообщение", response.getStatus() == Status.OK.getStatusCode());
+    }
+}
